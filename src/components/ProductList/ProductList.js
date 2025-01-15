7f112028ad203b97a9f57ffbc01e24bb8ac4f597
alisherkhann <sherkhanali032@gmail.com>
2025-01-15 12:52:58 +0530

    fix position of cards

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,7 +4,7 @@ import products from "../../assets/products";
 
 const ProductList = () => {
   return (
-    <div className="p-4 min-h-screen absolute top-[80vh] max-w-7xl mx-auto w-full">
+    <div className="p-4 min-h-screen absolute top-[80vh]  mx-auto w-full">
       <div className="flex flex-wrap items-center justify-center gap-4">
         {products.map((product) => (
           <div className="w-[220px]">
